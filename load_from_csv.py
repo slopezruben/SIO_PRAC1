66e1e9025c86947ff5e08ccc82fbfed6af8f8255
Ruben <slopez.rubent@gmail.com>
2023-02-28 21:14:39 +0100

    parte 1 final

--- load_from_csv.py
+++ load_from_csv.py
@@ -1,36 +1,62 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import pymysql
+import os
 #pymysql.install_as_MySQLdb()
 
 #Table Keys ['Host', 'Listing', 'Neighborhood']
-def join_host_list(listingTable, hostTable):
-    joinedListingTable = listingTable.assign(host_id=hostTable['host_id'] )
+def join_host_list(jlistingTable, jhostTable):
+    joinedListingTable = jlistingTable.assign(host_id=jhostTable['host_id'] )
     return joinedListingTable
 
+directorio = 'dataset'
+ciudades = []
+id_gen = 1
+
 engine = create_engine('mysql+pymysql://ruben:ferrari_18@localhost/sio_db')
-diccionario = pd.read_csv('dataset/diccionario.csv')
+diccionario = pd.read_csv('diccionario.csv')
 diccionario = diccionario[['Field', 'Ignore','Table']].dropna()
-df = pd.read_csv('dataset/mallorca.csv')
 
-print(diccionario)
+def get_tables(path):
+    df = pd.read_csv(f'dataset/{path}.csv')
 
-grupos_diccionario = diccionario.groupby(by=['Table'])
-df_procesados = {}
+    grupos_diccionario = diccionario.groupby(by=['Table'])
+    df_procesados = {}
 
-for table, grupo in grupos_diccionario:    
-    campos = []
-    for index, row in grupo.iterrows():
-        campos.append(row['Field'])
-    df_procesados[table]=df.loc[:,campos]
+    for table, grupo in grupos_diccionario:    
+        campos = []
+        for index, row in grupo.iterrows():
+            campos.append(row['Field'])
+        df_procesados[table]=df.loc[:,campos]
 
 
-listingTable = df_procesados.get('Listing')
-hostTable = df_procesados.get('Host')
-neighboorhoodTable = df_procesados.get('Neighborhood')
+    subListingTable = df_procesados.get('Listing')
+    subHostTable = df_procesados.get('Host')
+    subNeighborhoodTable = df_procesados.get('Neighborhood')
 
-df_procesados.update({'Listing': join_host_list(listingTable, hostTable)})
+    subListingTable['city_id'] = id_gen-1
+    df_procesados.update({'Listing': join_host_list(subListingTable, subHostTable)})
+    return subListingTable, subHostTable, subNeighborhoodTable
 
-for name, table in df_procesados.items():
-    table.to_sql(name, con=engine, if_exists='replace',index=False)
+for archivo in os.listdir(directorio):
+    if os.path.isfile(os.path.join(directorio,archivo)):
+        ciudad, extension = os.path.splitext(archivo)
+        ciudades.append((ciudad,id_gen))
+        id_gen += 1
+    subListingTable,subHostTable,subNeighborhoodTable = get_tables(ciudad)
+    if id_gen == 2 :
+        listingTable = subListingTable
+        hostTable = subHostTable
+        neighborhoodTable = subNeighborhoodTable
+    else:
+        listingTable = pd.concat([listingTable, subListingTable])
+        hostTable = pd.concat([hostTable, subHostTable])
+        neighborhoodTable = pd.concat([neighborhoodTable, subNeighborhoodTable])
 
+cityTable = pd.DataFrame(ciudades, columns=['ciudad', 'id'])
+print(cityTable)
+
+listingTable.to_sql('Listing', con=engine, if_exists='replace',index=False)
+hostTable.to_sql('Host', con=engine, if_exists='replace',index=False)
+neighborhoodTable.to_sql('Neighborhood', con=engine, if_exists='replace',index=False)
+cityTable.to_sql('City', con=engine, if_exists='replace',index=False)
