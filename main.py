0dc70a9169dbdd7a442bc73d6ffc6cc18faa66df
Ruben <slopez.rubent@gmail.com>
2023-02-28 16:39:06 +0100

    funciona la base de datos

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import pymysql
+pymysql.install_as_MySQLdb()
 
+#Table Keys ['Host', 'Listing', 'Neighborhood']
+def join_host_list(listingTable, hostTable):
+    joinedListingTable = listingTable.assign(host_id=hostTable['host_id'] )
+    return joinedListingTable
+
+engine = create_engine('mysql://ruben:ferrari_18@localhost/sio_db')
 diccionario = pd.read_csv('dataset/diccionario.csv')
 diccionario = diccionario[['Field', 'Ignore','Table']].dropna()
 df = pd.read_csv('dataset/mallorca.csv')
@@ -8,13 +16,21 @@ df = pd.read_csv('dataset/mallorca.csv')
 print(diccionario)
 
 grupos_diccionario = diccionario.groupby(by=['Table'])
-df_procesados = []
+df_procesados = {}
 
-for field, grupo in grupos_diccionario:    
+for table, grupo in grupos_diccionario:    
     campos = []
     for index, row in grupo.iterrows():
         campos.append(row['Field'])
-    df_procesados.append(df.loc[:,campos])
+    df_procesados[table]=df.loc[:,campos]
 
-for cositas in df_procesados:
-    print(cositas)
+
+listingTable = df_procesados.get('Listing')
+hostTable = df_procesados.get('Host')
+neighboorhoodTable = df_procesados.get('Neighborhood')
+
+df_procesados.update({'Listing': join_host_list(listingTable, hostTable)})
+
+for name, table in df_procesados.items():
+    table.to_sql(name, con=engine, if_exists='replace',index=False)
+
