a099bb298bb819968fdf6b49dce55df320178e98
Ruben <slopez.rubent@gmail.com>
2023-02-28 19:56:13 +0100

    load and get

---
+++ get_from_db.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import pymysql
+#pymysql.install_as_MySQLdb()
+
+engine = create_engine('mysql+pymysql://ruben:ferrari_18@localhost/sio_db')
+
+with engine.connect() as conn:
+    listingTable = pd.read_sql('Listing', con=conn)
+    hostTable = pd.read_sql('Host', con=conn)
+    neighborhoodTable = pd.read_sql('Neighborhood', con=conn)
+
+print(listingTable)
+print(hostTable)
+print(neighborhoodTable)
